a2915ea057645c483b0aceff93b103216ac121d4
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-21 19:37:49 +0530

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,8 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
-    APP_ID = 
-    API_HASH = ""
-    OWNER_ID = ""
-    AUTH = [556315181, -100088888, -10002252254]
+    BOT_TOKEN= "5135176286:AAGrkK58YfxWdg-VBA2jSJFWcKJ5AeXk3Fc"
+    APP_ID = "4738674"
+    API_HASH = "f2be74eaa9b1cb32498f45d04e4dbb54"
+    OWNER_ID = "1266733572"
+    AUTH = [-1001265756788]
